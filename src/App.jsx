33feef72bfadab5649ed4a8e89a65d7a600f1857
“Russel <russeldanielpaul@gmail.com>
2025-08-19 18:45:31 +0530

    vercel amalytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Resume from './pages/Resume';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
